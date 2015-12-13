cd0a302cafdbe8cd2880a155200755e5d2f5cc11
Markm <lordmarkm@gmail.com>
2015-12-14 00:49:50 +0800

    switchign user brand selection to checkbox

--- tls-app/src/main/webapp/app/user/controller/UserUpdateController.js
+++ tls-app/src/main/webapp/app/user/controller/UserUpdateController.js
@@ -1,14 +1,18 @@
 define(function () {
-  return ['$scope', '$state', 'brands', 'UserService', function ($scope, $state, brands, UserService) {
+  return ['$scope', '$state', 'user', 'brands', 'UserService', function ($scope, $state, user, brands, userBrands, UserService) {
 
-    $scope.user = {
+    $scope.user = user || {
       roles: ['ROLE_POS']
     };
     $scope.updateUserRequest = {
-        user: $scope.user
+        user: $scope.user,
+        brands: []
     };
     $scope.roles = $scope.isAuthorized('ROLE_ADMIN') ? ['ROLE_BRAND_MANAGER', 'ROLE_POS'] : ['ROLE_POS'];
     $scope.brands = brands;
+
+    //Set brands
+    console.debug('mother fucks' + userBrands);
 
     $scope.save = function () {
       UserService.create($scope.updateUserRequest, function (savedUser) {
--- tls-app/src/main/webapp/app/user/resolve/UserUpdateResolve.js
+++ tls-app/src/main/webapp/app/user/resolve/UserUpdateResolve.js
@@ -1,5 +1,12 @@
 define(function () {
   return {
+    user: ['UserService', '$stateParams', function (UserService, $stateParams) {
+      if ($stateParams.userCode) {
+        return UserService.get({userCode: $stateParams.userCode});
+      } else {
+        return undefined;
+      }
+    }],
     //The brands that the user may be assigned to
     brands: ['$q', 'BrandService', function ($q, BrandService) {
       var brands = $q.defer();
@@ -7,6 +14,18 @@ define(function () {
         brands.resolve(response.data);
       });
       return brands.promise;
+    }],
+    //The brands that the user is already assigned to
+    userBrands: ['$q', '$stateParams', 'BrandService', function ($q, $stateParams, BrandService) {
+      var brands = $q.defer();
+      if (!$stateParams.userCode) {
+        brands.resolve([]);
+      } else {
+        BrandService.query({userCode: $stateParams.userCode, page: 1, count: 9999}, function (response) {
+          brands.resolve(response);
+        });
+      }
+      return brands.$promise;
     }]
   };
 });
