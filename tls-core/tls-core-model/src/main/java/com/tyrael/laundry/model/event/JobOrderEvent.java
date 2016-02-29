04c167b4847c8b196ae896a534ca850f0dcddecf
mmartinez <mmartinez@1T6B162-Inspiron-5558>
2016-02-29 19:43:03 +0800

    events

--- tls-core/tls-core-model/src/main/java/com/tyrael/laundry/model/event/JobOrderEvent.java
+++ tls-core/tls-core-model/src/main/java/com/tyrael/laundry/model/event/JobOrderEvent.java
@@ -6,6 +6,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.tyrael.laundry.model.joborder.JobOrder;
+import com.tyrael.laundry.reference.EventType;
 
 @Entity(name = "event_job_order")
 @DiscriminatorValue("JOB_ORDER")
@@ -14,6 +15,8 @@ public class JobOrderEvent extends TlsEvent {
     @ManyToOne
     @JoinColumn(name = "job_order_id")
     private JobOrder jobOrder;
+
+    private EventType eventType;
 
     public JobOrderEvent(JobOrder jobOrder) {
         
