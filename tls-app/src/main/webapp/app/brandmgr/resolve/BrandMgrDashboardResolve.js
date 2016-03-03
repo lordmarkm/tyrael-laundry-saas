d29f6b316f102545233e2460e6af54a471394141
Markm <lordmarkm@gmail.com>
2016-03-03 23:10:46 +0800

    Apply event feed to brand mgr

--- tls-app/src/main/webapp/app/brandmgr/resolve/BrandMgrDashboardResolve.js
+++ tls-app/src/main/webapp/app/brandmgr/resolve/BrandMgrDashboardResolve.js
@@ -2,6 +2,9 @@ define(function () {
   return {
     events: ['EventService', function (EventService) {
       return EventService.get({page:1, sort: 'dateCreated,DESC'});
+    }],
+    dashboard: ['DashboardService', function (DashboardService) {
+      return DashboardService.get();
     }]
   };
 });
