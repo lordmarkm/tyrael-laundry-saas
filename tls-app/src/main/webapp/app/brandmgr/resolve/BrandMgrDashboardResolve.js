89542b700f89ccc4ccf2d50c738fe2fb44681302
Markm <lordmarkm@gmail.com>
2016-03-05 14:48:52 +0800

    Paginate brand mgr dashboard resolve

--- tls-app/src/main/webapp/app/brandmgr/resolve/BrandMgrDashboardResolve.js
+++ tls-app/src/main/webapp/app/brandmgr/resolve/BrandMgrDashboardResolve.js
@@ -1,7 +1,7 @@
 define(function () {
   return {
     events: ['EventService', function (EventService) {
-      return EventService.get({page:1, sort: 'dateCreated,DESC'});
+      return EventService.get({page:1, count: 5, sort: 'dateCreated,DESC'});
     }],
     dashboard: ['DashboardService', function (DashboardService) {
       return DashboardService.get();
