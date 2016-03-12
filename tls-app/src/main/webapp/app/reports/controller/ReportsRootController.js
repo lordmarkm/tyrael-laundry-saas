15704de9caa510643e93bf6e6ec448ebde99138a
lordmarkm <lordmarkm@gmail.com>
2016-03-12 23:56:54 +0800

    sgsdgsdfg

--- tls-app/src/main/webapp/app/reports/controller/ReportsRootController.js
+++ tls-app/src/main/webapp/app/reports/controller/ReportsRootController.js
@@ -2,5 +2,29 @@ define(function () {
   return ['$scope',
           function ($scope) {
 
+    $scope.saikuRepositoryService = $resource('/saiku/rest/saiku/admin/repository');
+
+    $scope.saikuFile = 'wait.saiku';
+
+    $scope.saikuClient.execute({
+      file: $scope.saikuFile,
+      htmlObject: "#saiku-chart",
+      render: "chart",
+      mode: "bar",
+      zoom: true,
+      chartDefinition: {
+        height: 400,
+        colors: ["red", "blue", "green", "yellow"],
+        baseAxisSize: "300px",
+        baseAxisSizeMax : "200"
+      }
+    });
+
+    $scope.saikuClient.execute({
+      file: $scope.saikuFile,
+      htmlObject: "#saiku-table",
+      render: "table"
+    });
+
   }];
 });
